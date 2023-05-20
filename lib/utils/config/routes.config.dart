0b0142c5497d28ced80192d460ede9d1e91fd239
Milzam Hibatullah <hmilzam@gmail.com>
2023-05-20 21:44:42 +0700

    create Approutes class

---
+++ lib/utils/config/routes.config.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:music_app_player/view/home.screen.view.dart';
+
+///manage all navigation routes here
+class AppRoutes {
+  ///define static const route name below
+  static const String home = '/';
+
+
+
+  ///generate route function that returns Route<dynamic>
+  ///Apply this function onGenerateRoutes
+  static Route<dynamic> generateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomeScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
